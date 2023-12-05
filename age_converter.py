be68d351f6438917a31c310d9d64938fcfc519a9
Paula Prieto <paueprieto@gmail.com>
2023-12-05 19:50:45 +1100

    Update age_converter.py

--- age_converter.py
+++ age_converter.py
@@ -2,7 +2,7 @@
 # Program that converts age from years to days
 
 # input for age, converts to integer
-age_in_years = int(input("Enter your age in years "))
+age_in_years = int(input("Enter your age in years: "))
 
 # converts years to days
 age_in_days = age_in_years * 365
@@ -10,8 +10,8 @@ age_in_days = age_in_years * 365
 # minimum age validation
 if age_in_years > 0:
 
-    # outputs age in age_in_days
-    print("Your age in days is roughly", age_in_days, "days old")
+    # outputs age in age_in_dayspython
+    print("You are roughly", age_in_days, "days old")
 
 else:
     # if validation fails
